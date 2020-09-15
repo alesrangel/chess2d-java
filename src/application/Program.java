81b034769eba1eb0093a372e156049a5df8eaedb
Ales Rangel <alesrangel15@gmail.com>
2020-09-15 19:59:30 -0300

    little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		ChessMatch chessMatch = new ChessMatch();
-		UI.PrintBoard(chessMatch.getPieces());
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 }
